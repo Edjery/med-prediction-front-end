f33bbf83343a5c7ae3dea0e36758db946068b241
Edjery Gumbao <edisgumbao@gmail.com>
2024-02-01 09:43:15 +0800

    Finished Landing Page and installed axios

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, { useState } from "react";
+import "./App.css";
+import logo from "./logo.svg";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [predictionBest, setPredictionBest] = useState("");
+
+  const handleInputChange = (event) => {
+    setInputText(event.target.value);
+  };
+
+  const handlePrediction = async () => {
+    try {
+      const payload = { text: inputText };
+      console.log("Request payload:", payload);
+
+      const response = await axios.post(
+        "http://localhost:8000/predict",
+        payload
+      );
+      console.log(response.data);
+      setPredictionBest(response.data.prediction_best);
+    } catch (error) {
+      console.error("Error predicting:", error);
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>spaCy FastAPI React App</h2>
       </header>
+
+      <body className="App-body">
+        <textarea
+          className="App-textarea"
+          rows="10"
+          cols="80"
+          value={inputText}
+          onChange={handleInputChange}
+          placeholder="Enter text..."
+        />
+        <div className="App-box">
+          <button className="App-button" onClick={handlePrediction}>
+            Predict
+          </button>
+          <p>Prediction using Best Model: {predictionBest}</p>
+        </div>
+      </body>
     </div>
   );
 }
